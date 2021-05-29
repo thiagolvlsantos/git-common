b1249ea65de2805af06b158a08abdfbfab0c652c
thiagolvlsantos <thiago.lvl.santos@gmail.com>
2021-05-29 00:19:47 -0300

    Unit testing.

--- src/main/java/io/github/thiagolvlsantos/git/commons/file/FileUtils.java
+++ src/main/java/io/github/thiagolvlsantos/git/commons/file/FileUtils.java
@@ -1,20 +1,33 @@
 package io.github.thiagolvlsantos.git.commons.file;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
 
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class FileUtils {
 
-	public static boolean mkdirs(File dir) {
-		return dir.mkdirs();
+	public static void write(File f, String content) throws IOException {
+		if (!f.exists() && !prepare(f)) {
+			throw new IOException("Unable to setup file structure.");
+		}
+		Files.write(f.toPath(), String.valueOf(content).getBytes(), StandardOpenOption.CREATE, StandardOpenOption.WRITE,
+				StandardOpenOption.TRUNCATE_EXISTING);
+	}
+
+	public static boolean prepare(File f) {
+		File dir = f.getParentFile();
+		return dir.exists() || dir.mkdirs();
 	}
 
 	public static boolean delete(File f) {
 		boolean ok = true;
 		if (f.isDirectory()) {
-			for (File c : f.listFiles()) {
+			File[] children = f.listFiles();
+			for (File c : children) {
 				ok = ok & delete(c);
 			}
 		}
---
+++ src/test/java/io/github/thiagolvlsantos/git/commons/file/FileUtilsTest.java
@@ -0,0 +1,42 @@
+package io.github.thiagolvlsantos.git.commons.file;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class FileUtilsTest {
+
+	@Test
+	public void testCreate() throws IOException {
+		File file = new File("target", "git-commons" + File.separator + "test.txt");
+		FileUtils.write(file, "OK"); // not exists
+		FileUtils.write(file, "OK"); // already exists
+	}
+
+	@Test
+	public void testDelete() throws IOException {
+		File file = new File("target", "git-commons" + File.separator + "test.txt");
+		FileUtils.write(file, "OK");
+		assertTrue(FileUtils.delete(file.getParentFile()));
+	}
+
+	@Test
+	public void testDeleteChildren() throws IOException {
+		File root = new File("target", "git-commons");
+
+		File file = new File(root, "test.txt");
+		FileUtils.write(file, "OK");
+
+		File sub1 = new File(root, "sub1");
+		File child1 = new File(sub1, "child.txt");
+		FileUtils.write(child1, "Child");
+
+		File empty = new File(root, "empty");
+		assertTrue(empty.mkdirs());
+
+		assertTrue(FileUtils.delete(root));
+	}
+}
